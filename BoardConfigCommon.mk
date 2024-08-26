3d5e5ad1547bf7a769131b3cba3cce38f96f0ca1
Christopher Ferris <cferris@google.com>
2024-08-26 23:44:15 +0100

    universal9830: Enable 32 bit building, but only 64 bit zygote.
    Bug: 229787331
    Test: Build/Boot in progress.
    Change-Id: I4686b567fd96a6cd7c3f6e269f0424811c0b3d6a
    Signed-off-by: ExtremeXT <extremextdev@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -153,6 +153,9 @@ BOARD_AVB_RECOVERY_KEY_PATH := external/avb/test/data/testkey_rsa4096.pem
 BOARD_AVB_RECOVERY_ROLLBACK_INDEX := 1
 BOARD_AVB_RECOVERY_ROLLBACK_INDEX_LOCATION := 1
 
+# Zygote
+ZYGOTE_FORCE_64 := true
+
 # Call Samsung LSI board support package
 include hardware/samsung_slsi-linaro/config/BoardConfig9830.mk
 
