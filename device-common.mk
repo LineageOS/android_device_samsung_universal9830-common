711ca3bf5c5fa91105d238ccd68a32488fa394bc
ExtremeXT <extremextdev@gmail.com>
2025-11-27 20:32:00 +0200

    Revert "universal9830: build AdvancedDisplay"
    This reverts commit f77aa3371d3149054d80aa36db6bfe8354cb1534.
    
    Change-Id: I91f2721ba595feeb3724a7293ed1a34f68852de9
    Signed-off-by: ExtremeXT <extremextdev@gmail.com>

--- device-common.mk
+++ device-common.mk
@@ -21,10 +21,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
 
-# AdvancedDisplay (mDNIe)
-PRODUCT_PACKAGES += \
-    AdvancedDisplay 
-
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio.effect@7.0-impl:32 \
