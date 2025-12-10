661492240a68542c610ed385987a7c83d19664dd
CmdCtrlDevic3 <cmdctrldevic3@proton.me>
2025-12-10 19:56:41 +0200

    universal9830: Move brightness overlays to Hubble
    Galaxy S20 FE (r8s) brightness table is different because of different display panel.
    
    Change-Id: I45fd6d576cffc0e9b294ea6e08e6059b173271f3
    Signed-off-by: CmdCtrlDevic3 <cmdctrldevic3@proton.me>
    (cherry picked from commit 086d3350ae362e1aa48f81384d223e29dacd0407)

--- device-hubble.mk
+++ device-hubble.mk
@@ -19,4 +19,11 @@ $(call inherit-product, device/samsung/universal9830-common/device-common.mk)
 
 # NFC
 PRODUCT_COPY_FILES += \
+<<<<<<< HEAD   (2ebab9175d5854486dfd84c1e7764015c02a4ce8 universal9830: Move NFC config to Hubble)
     $(LOCAL_PATH)/hubble/configs/nfc/libnfc-sec-vendor.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-sec-vendor.conf
+=======
+    $(LOCAL_PATH)/hubble/configs/nfc/libnfc-sec-vendor.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-sec-vendor.conf
+
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/hubble/overlay
+>>>>>>> CHANGE (086d3350ae362e1aa48f81384d223e29dacd0407 universal9830: Move brightness overlays to Hubble)
