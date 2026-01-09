c7357adce233cc270a13168cc74750b4a2c3fcbd
ExtremeXT <extremextdev@gmail.com>
2026-01-09 14:03:36 +0200

    universal9830: Kill VPL plugin
    Reason: Causes provider crash on deinit.
    
    Change-Id: If60120f87108c4b1f2b97c6809570ce3a0fe426a
    Signed-off-by: ExtremeXT <extremextdev@gmail.com>

--- device-common.mk
+++ device-common.mk
@@ -412,6 +412,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libutils-v32
 
+PRODUCT_PACKAGES += \
+    libvpl
+
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
---
+++ libshims/libvpl/vpl.c
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2024 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <stdint.h>
+
+typedef struct VPL_FacesStr VPL_FacesStr;
+
+void createVpl() {
+}
+
+void destroyVpl() {
+}
+
+int vplHandleArray(uint64_t param_1, uint64_t param_2, long param_3, VPL_FacesStr *param_4, unsigned long *param_5) {
+    return 0;
+}
