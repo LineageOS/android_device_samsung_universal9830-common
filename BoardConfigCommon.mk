9b2e6e106528751b6042da87f2f1e2025595e7a4
ExtremeXT <extremextdev@gmail.com>
2026-01-22 22:12:04 +0200

    universal9830: Initial canvas support
    Change-Id: I030106b2f16301b784ba83116d88a0b36769f656
    Signed-off-by: ExtremeXT <extremextdev@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -67,6 +67,7 @@ TARGET_NO_BOOTLOADER := true
 TARGET_NO_RADIOIMAGE := true
 
 # Framework Matrix
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += $(COMMON_PATH)/device_framework_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += hardware/samsung/vintf/samsung_framework_compatibility_matrix.xml
 
 # Graphics
