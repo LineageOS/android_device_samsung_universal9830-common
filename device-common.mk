c64be52797e7094cb5efdbb361ba223985938b7e
ExtremeXT <extremextdev@gmail.com>
2026-01-22 16:12:04 +0200

    universal9830: Switch to Samsung Lineage Health Service
    Change-Id: If541edef66fbac695499d253c6418a6b63e24005
    Signed-off-by: ExtremeXT <extremextdev@gmail.com>

--- device-common.mk
+++ device-common.mk
@@ -160,17 +160,7 @@ $(call soong_config_set,samsungVars,target_keymaster4_library,//vendor/samsung/u
 
 # Lineage Health
 PRODUCT_PACKAGES += \
-    vendor.lineage.health-service.default
-
-$(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/power_supply/battery/charging_enabled)
-$(call soong_config_set,lineage_health,charging_control_charging_enabled,1)
-$(call soong_config_set,lineage_health,charging_control_charging_disabled,0)
-$(call soong_config_set_bool,lineage_health,charging_control_charging_bypass,true)
-$(call soong_config_set_bool,lineage_health,charging_control_charging_toggle,true)
-$(call soong_config_set_bool,lineage_health,charging_control_charging_deadline,false)
-$(call soong_config_set,lineage_health,fast_charge_node,/sys/class/sec/switch/afc_disable)
-$(call soong_config_set,lineage_health,fast_charge_value_none,1)
-$(call soong_config_set,lineage_health,fast_charge_value_fast_charge,0)
+    vendor.lineage.health-service.samsung
 
 # Livedisplay
 PRODUCT_PACKAGES += \
