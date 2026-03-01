b7eab123c60a2acf43d11e8abce934514ccc8aec
ExtremeXT <extremext@lineageos.org>
2026-03-02 00:48:51 +0200

    universal9830: Build OMX service explicitly
    This doesn't get built automatically anymore in 64-bit only builds.
    
    Change-Id: I6370b2be8ad853b239b365c68e1c83afddb0bc99
    Signed-off-by: ExtremeXT <extremext@lineageos.org>

--- device-common.mk
+++ device-common.mk
@@ -180,6 +180,7 @@ PRODUCT_PACKAGES += \
 
 # Media
 PRODUCT_PACKAGES += \
+    android.hardware.media.omx@1.0-service \
     libExynosOMX_Core \
     libExynosOMX_Resourcemanager \
     libOMX.Exynos.AVC.Decoder \
