2d387a2f4a5faa228cd09a7ec749c9f4a0dddf10
krazey <admin@krazey.de>
2026-05-29 01:37:41 +0200

    [DNM] universal9830: Setup IMS
    Change-Id: I835f2c7f61dc6fc415fc52ac0710cf7dba938773
    Signed-off-by: krazey <admin@krazey.de>
    Signed-off-by: ExtremeXT <extremext@lineageos.org>

--- device-common.mk
+++ device-common.mk
@@ -132,6 +132,13 @@ PRODUCT_PACKAGES += \
     android.hardware.health-service.samsung \
     android.hardware.health-service.samsung-recovery
 
+# IMS
+PRODUCT_PACKAGES += \
+    Iwlan \
+    PhhIms \
+    QualifiedNetworksService \
+    privapp-permissions-me.phh.ims.xml
+
 # Init
 PRODUCT_PACKAGES += \
     fstab.exynos990 \
@@ -264,6 +271,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.stepdetector.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.stepdetector.xml \
     frameworks/native/data/etc/android.hardware.strongbox_keystore.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.strongbox_keystore.xml \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml \
+    frameworks/native/data/etc/android.hardware.telephony.ims.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.ims.xml \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml \
--- extract-files.py
+++ extract-files.py
@@ -70,8 +70,6 @@ blob_fixups: blob_fixups_user_type = {
     'vendor/lib64/libeden_ud_gpu.so': blob_fixup()
         .replace_needed('libOpenCL.so', 'libGLES_mali.so')
         .add_needed('libeden_ud_cpu.so'),
-    'vendor/lib64/libsec-ril.so': blob_fixup()
-        .sig_replace('80 0E 40 F9 E1 03 16 AA 82 0C 80 52 E3 03 15 AA', '80 0E 40 F9 E1 03 16 AA 82 0C 80 52 03 00 80 D2'),
     'vendor/lib64/libsensorlistener.so': blob_fixup()
         .add_needed('libsensorndkbridge_shim.so'),
     'vendor/lib/libwvhidl.so': blob_fixup()
