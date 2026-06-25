736b8606c3237a26ff0520c9c75456b136615f75
Nolen Johnson <johnsonnolen@gmail.com>
2026-06-25 19:19:11 -0400

    universal9830: Stop disabling configstore
    * As of CP2A it's just dead and gone.
    
    Change-Id: I671b668c4bb9a967f779753233c2025a428c9421
    Signed-off-by: ExtremeXT <extremext@lineageos.org>

--- device-common.mk
+++ device-common.mk
@@ -88,10 +88,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     charger_res_images_vendor
 
-# Configstore
-PRODUCT_PACKAGES += \
-    disable_configstore
-
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.3.vendor:32 \
