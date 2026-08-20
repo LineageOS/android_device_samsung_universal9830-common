d46f953800d9cccd6b1f7401c037d12fd9aa805c
ExtremeXT <extremext@lineageos.org>
2026-08-21 02:12:53 +0300

    universal9830: Switch to OSS GNSS service
    Change-Id: I0b280dadc1320afc716a69af6807cf4e11db43fb
    Signed-off-by: ExtremeXT <extremext@lineageos.org>

--- device-common.mk
+++ device-common.mk
@@ -116,8 +116,7 @@ PRODUCT_PACKAGES += \
 
 # GNSS
 PRODUCT_PACKAGES += \
-    android.frameworks.sensorservice@1.0.vendor:64 \
-    android.hardware.gnss@2.1.vendor:64 \
+    android.hardware.gnss@2.1-service.samsung \
     init.gps.rc
 
 PRODUCT_COPY_FILES += \
