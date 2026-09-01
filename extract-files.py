472ce07189a395f0b17ff10fb78daeb7f07e8ca8
Mathias Gluszczynski <admin@krazey.de>
2026-09-01 15:29:02 +0300

    universal9830: Switch to a shim to null SMSC
    Samsung's legacy RIL expects a null SMSC for CS SMS requests, while
    Android needs a valid GET_SMSC_ADDRESS response for IMS SMS.
    
    Wrap libsec-ril and replace only its onRequest callback in place,
    preserving the vendor callback table, socket IDs, and RIL_SAP_Init.
    Clear the SMSC only for SEND_SMS and SEND_SMS_EXPECT_MORE.
    
    Remove the stale OnGetSmscAddressDone blob fixup that corrupted its
    response pointer. Update the renamed blob fixups and post-fixup hash.
    
    Change-Id: Ia8e4d6830ccf055809ef092ceff20afc89ed915d
    Signed-off-by: ExtremeXT <extremext@lineageos.org>

--- extract-files.py
+++ extract-files.py
@@ -74,8 +74,6 @@ blob_fixups: blob_fixups_user_type = {
         .clear_symbol_version('AHardwareBuffer_describe')
         .clear_symbol_version('AHardwareBuffer_lock')
         .clear_symbol_version('AHardwareBuffer_unlock'),
-    'vendor/lib64/libsec-ril.so': blob_fixup()
-        .sig_replace('80 0E 40 F9 E1 03 16 AA 82 0C 80 52 E3 03 15 AA', '80 0E 40 F9 E1 03 16 AA 82 0C 80 52 03 00 80 D2'),
     'vendor/lib64/libsensorlistener.so': blob_fixup()
         .add_needed('libsensorndkbridge_shim.so'),
     'vendor/lib/libwvhidl.so': blob_fixup()
